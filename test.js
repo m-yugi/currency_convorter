844f7031b917339ce34e233b913e9cb10be875d7
yugi <msaiyugandhar333@gmail.com>
2023-04-23 16:12:08 +0530

    added a login page to the currency converter page

--- test.js
+++ test.js
@@ -170,37 +170,7 @@ let symbols = {
   ZMW: "Zambian Kwacha",
   ZWL: "Zimbabwean Dollar",
 };
-var myHeaders = new Headers();
-myHeaders.append("apikey", "ObjbzWNbGGvckl1UO2qDXc0qTqp4lI06");
-var requestOptions = {
-  method: "GET",
-  headers: myHeaders,
-  redirect: "follow",
-};
-function get_data() {
-  let to_data = document.getElementById("to_curr").value;
-  let from_data = document.getElementById("from_curr").value;
-  let amount = document.getElementById("amount").value;
-  // console.log(amount);
-  // console.log(from_data)
-  fetch("https://api.apilayer.com/exchangerates_data/latest", requestOptions)
-    .then((response) => response.json())
-    .then((result) => {
-      document.getElementById(
-        "result"
-      ).innerHTML = `${amount} ${from_data} = ${(
-        amount *
-        (result.rates[to_data] / result.rates[from_data])
-      ).toFixed(3)} ${to_data}`;
-      document.getElementById(
-        "from_curr_rate"
-      ).innerHTML = `1 ${from_data} = ${(
-        result.rates[to_data] / result.rates[from_data]
-      ).toFixed(3)} ${to_data}`;
-
-      document.getElementById("to_curr_rate").innerHTML = `1 ${to_data} = ${(
-        result.rates[from_data] / result.rates[to_data]
-      ).toFixed(3)} ${from_data}`;
-    })
-    .catch((error) => console.log("error", error));
+for (const [key, value] of Object.entries(symbols)) {
+    
+  console.log(key, value);
 }
